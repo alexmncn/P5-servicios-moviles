acb732384b903911e0a707158821e262a9d7c35c
alexmncn <118453946+alexmncn@users.noreply.github.com>
2024-12-10 23:52:06 +0100

    Working with firebase. Analytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -9,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.alexmncn.p5_servicios_moviles"
         minSdk = 21
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
@@ -37,6 +38,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.ui)
+    implementation(libs.firebase.analytics)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/alexmncn/p5_servicios_moviles/MainActivity.java
+++ app/src/main/java/com/alexmncn/p5_servicios_moviles/MainActivity.java
@@ -3,12 +3,21 @@ package com.alexmncn.p5_servicios_moviles;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        Bundle bundle = new Bundle();
+
+        bundle.putString("message", "Integracion de Firebase Analytics");
+        mFirebaseAnalytics.logEvent("InitScreen", bundle);
     }
 }
